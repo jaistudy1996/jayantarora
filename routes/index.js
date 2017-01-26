c9cdc8ac623c3bb5d789ee454e32db79d8d22009
Jayant Arora <jaistudy1996@gmail.com>
2017-01-25 22:31:49 -0500

    Add git activity stream widget and remove unncessary code

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/resume', function(req, res, next){
+	res.redirect('/resume.pdf');
+});
+
 module.exports = router;
